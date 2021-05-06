e4c4a53d9a04cfeba1306708685bf1f4668e34c4
Esther <esther191299@gmail.com>
2021-05-06 05:21:23 +0200

    added onActivityResult method

--- Novi Medewerker vd Maand App/app/src/main/java/com/esther/noviMedewerkerMaandApp/MainActivity.java
+++ Novi Medewerker vd Maand App/app/src/main/java/com/esther/noviMedewerkerMaandApp/MainActivity.java
@@ -3,7 +3,9 @@ package com.esther.noviMedewerkerMaandApp;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.esther.noviMedewerkerMaandApp.databinding.ActivityMainBinding;
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        getSupportActionBar().hide();
+
 //        opens gallery
         binding.openGalleryButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +33,16 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, IMAGE_REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_REQUEST_CODE){
+            if (data.getData() != null) {
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
