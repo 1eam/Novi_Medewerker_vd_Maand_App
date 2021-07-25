61d79caf3949712a1d03f6a54cdb32dc491bbaae
Esther <esther191299@gmail.com>
2021-07-25 16:07:45 +0200

    removed unused imports

--- Novi Medewerker vd Maand App/app/src/main/java/com/esther/noviMedewerkerMaandApp/ResultActivity.java
+++ Novi Medewerker vd Maand App/app/src/main/java/com/esther/noviMedewerkerMaandApp/ResultActivity.java
@@ -6,16 +6,11 @@
 package com.esther.noviMedewerkerMaandApp;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.esther.noviMedewerkerMaandApp.databinding.ActivityResultBinding;
